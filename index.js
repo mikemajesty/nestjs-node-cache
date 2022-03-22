56ba06599701a77d3b9444d9ee9fe09ff392d42d
Mike Lima <mike.rodrigues.lima@gmail.com>
2022-03-22 20:50:03 -0300

    feat(version): add npm version

--- index.js
+++
@@ -1,8 +0,0 @@
-const { CacheModule } = require('./dist/src/cache/module');
-const { ICacheService } = require('./dist/src/cache/adpater');
-
-
-module.exports = {
-  CacheModule,
-  ICacheService
-}
---
+++ index.ts
@@ -0,0 +1,8 @@
+const { CacheModule } = require('./src/cache/module');
+const { ICacheService } = require('./src/cache/adpater');
+
+
+module.exports = {
+  CacheModule,
+  ICacheService
+}
