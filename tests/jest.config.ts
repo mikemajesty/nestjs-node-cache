f2950b3a719cb90fcddd0b4af0d75543ba7b8d4b
Mike Lima <mike.rodrigues.lima@gmail.com>
2022-03-23 21:53:12 -0300

    feat(npm version): add npm version

--- tests/jest.config.ts
+++ tests/jest.config.ts
@@ -1,14 +1,15 @@
 import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
+import { compilerOptions } from '../tsconfig.json';
 
 export default {
   moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: "../src",
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
   coveragePathIgnorePatterns: ['dist', 'node_modules', 'coverage'],
-  setupFilesAfterEnv: ['./tests/init.ts'],
+  setupFilesAfterEnv: ['../tests/init.ts'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
   testEnvironment: 'node'
 };
